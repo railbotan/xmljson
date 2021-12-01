cc45de2989207d94bce5a4fa01a1801cf111fdb5
Arzu Rzaeva <rzaeva-010203@mail.ru>
2021-12-01 13:52:54 +0500

    Add methods in news.py to create full_news.json, add full_news.json

--- news.py
+++ news.py
@@ -3,6 +3,12 @@ from urllib.request import urlopen
 import json
 
 
+def get_first_branch():
+    data = urlopen('https://lenta.ru/rss').read().decode('utf8')
+    root = ET.fromstring(data)
+    return root[0]
+
+
 def get_news(ch: ET.Element):
     news = []
     for i in ch.findall('item'):
@@ -13,17 +19,22 @@ def get_news(ch: ET.Element):
     return news
 
 
-def get_first_branch():
-    data = urlopen('https://lenta.ru/rss').read().decode('utf8')
-    root = ET.fromstring(data)
-    return root[0]
+def get_full_news(ch: ET.Element):
+    full_news = []
+    for i in ch.findall('item'):
+        fields_dict = {}
+        for f in i:
+            fields_dict[f.tag] = f.text
+        full_news.append(fields_dict)
+    return full_news
 
 
-def save_json(ch):
-    json_file = json.dumps(get_news(ch), ensure_ascii=False).encode('utf8')
-    with open("news.json", 'wb') as f:
+def save_json(file_name, news):
+    json_file = json.dumps(news, ensure_ascii=False).encode('utf8')
+    with open(file_name, 'wb') as f:
         f.write(json_file)
 
 
 channel = get_first_branch()
-save_json(channel)
+# save_json("news.json", get_news(channel))
+save_json("full_news.json", get_full_news(channel))
