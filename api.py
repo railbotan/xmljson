b926cc854cac08f76ef6f76cfa0ce43e9f391ca8
alyonafilyaeva <74866828+alyonafilyaeva@users.noreply.github.com>
2021-12-03 07:30:04 +0500

    Update api.py

--- api.py
+++ api.py
@@ -7,8 +7,8 @@ def convert_date(date):
     return datetime.strptime(date['timestamp'], '%Y-%m-%dT%H:%M:%SZ').date()
 
 def get_revisions(revisions):
-    for key, group_items in groupby(revisions, key = convert_date):
-        print(key, sum(1 for i in group_items))
+    for key, items in groupby(revisions, key = convert_date):
+        print(key, sum(1 for i in items))
 
 # Градский
 url_Gradsky = 'https://ru.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&rvlimit=500&titles=%D0%93%D1%80%D0%B0%D0%B4%D1%81%D0%BA%D0%B8%D0%B9,_%D0%90%D0%BB%D0%B5%D0%BA%D1%81%D0%B0%D0%BD%D0%B4%D1%80_%D0%91%D0%BE%D1%80%D0%B8%D1%81%D0%BE%D0%B2%D0%B8%D1%87'
