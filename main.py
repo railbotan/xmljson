f4317b62fc94979aba8fca9c2041ab4f6c62a1f3
CyberMagic404 <81950751+CyberMagic404@users.noreply.github.com>
2021-12-05 11:57:43 +0500

    Second task.

--- main.py
+++ main.py
@@ -2,21 +2,27 @@ import xml.etree.ElementTree as ET
 from urllib.request import urlopen
 import json
 
+#Старая версия get_news
+# def get_news(channel):
+#     news = []
+#     for c in channel.findall('item'):
+#         news.append({'pubDate': c.find('pubDate').text, 'title': c.find('title').text})
+#     return news
+
 def get_news(channel):
     news = []
     for c in channel.findall('item'):
-        news.append({'pubDate': c.find('pubDate').text, 'title': c.find('title').text})
+        item_dict = {}
+        for i in c:
+            item_dict[i.tag] = i.text
+        news.append(item_dict)
     return news
-
-def create_json_file(channel):
-    json_file = json.dumps(get_news(channel), ensure_ascii=False, indent=4).encode('utf8')
-    with open("news.json", 'wb') as f:
-        f.write(json_file)
 
 def main():
     data = urlopen('https://lenta.ru/rss').read().decode('utf8')
     root = ET.fromstring(data)
-    create_json_file(root[0])
+    with open('news.json', 'w', encoding='utf-8') as f:
+        json.dump(get_news(root[0]), f, ensure_ascii=False, indent=4)
 
 if __name__ == "__main__":
     main()
