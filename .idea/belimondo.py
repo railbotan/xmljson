43b53a86e04f0755a8f70c827b00710045c90c11
Straordinariu <sasha4983@mail.ru>
2021-12-06 05:29:44 +0500

    done

---
+++ .idea/belimondo.py
@@ -0,0 +1,9 @@
+from itertools import groupby
+from json import loads
+from urllib.request import urlopen
+
+url = 'https://ru.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&rvlimit=500&titles=%D0%91%D0%B5%D0%BB%D1%8C%D0%BC%D0%BE%D0%BD%D0%B4%D0%BE,_%D0%96%D0%B0%D0%BD-%D0%9F%D0%BE%D0%BB%D1%8C'
+data = loads(urlopen(url).read().decode('utf8'))
+
+data_belimondo = groupby([i['timestamp'][:10] for i in data['query']['pages']['192203']['revisions']])
+[print(d, len(list(e))) for d, e in data_belimondo]
